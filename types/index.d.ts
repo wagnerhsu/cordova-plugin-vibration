02156a99f14f951048999bf6af341bde43fb1769
Wagner Xu <wagnerhsu@hotmail.com>
2024-09-21 20:07:03 +0800

    feat: add android implementation again

--- types/index.d.ts
+++ types/index.d.ts
@@ -20,3 +20,27 @@ interface Navigator {
      */
     vibrate(time: number[]): void;
 }
+
+interface Notification {
+	/**
+	 * Vibrates the device for the specified amount of time.
+	 * @param time Milliseconds to vibrate the device. Ignored on iOS.
+     * @deprecated
+	 */
+    vibrate(time: number): void;
+    /**
+     * Vibrates the device with a given pattern.
+     * @param number[] pattern Pattern with which to vibrate the device.
+     *                         The first value - number of milliseconds to wait before turning the vibrator on.
+     *                         The next value - the number of milliseconds for which to keep the vibrator on before turning it off.
+     * @param number  repeat   Optional index into the pattern array at which to start repeating (will repeat until canceled),
+     *                         or -1 for no repetition (default).
+     * @deprecated
+     */
+    vibrateWithPattern(pattern: number[], repeat: number): void;
+    /**
+     * Immediately cancels any currently running vibration.
+     * @deprecated
+     */
+    cancelVibration(): void;
+}
